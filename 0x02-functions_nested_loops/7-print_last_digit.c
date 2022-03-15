c505bcb91650f04c2fc9accab9b2ebf9ab42100c
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-15 23:07:12 +0300

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print the absolute value of numbers
+ *
+ * @r: number that was changed with abs
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int r)
+{
+int i;
+i = r % 10;
+return (i);
+}
