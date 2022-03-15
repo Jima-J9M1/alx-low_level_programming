6d2b21aea935cd8258c0f34f9a1ceefe53bf96e2
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-15 22:26:38 +0300

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_sign - determine the sign on each numbers
+ *
+ * @n: is the sign number
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+return (1);
+}
+else  if (n < 0)
+{
+_putchar('-');
+_putchar(',');
+return (-1);
+}
+if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+_putchar(',');
+return (0);
+}
