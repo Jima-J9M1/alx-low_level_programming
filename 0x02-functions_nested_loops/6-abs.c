127bdc481105e69032f0fac2bafecf8f7ceedb47
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-15 22:57:39 +0300

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of numbers
+ *
+ * @r: number that was changed with abs
+ * Return: Always 0 (Success)
+ */
+int _abs(int r)
+{
+if (r >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
