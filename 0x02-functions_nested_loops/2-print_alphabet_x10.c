a937a7e885d2ea5bbe00efe4648edc32bd63627d
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-15 20:28:16 +0300

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - Determines either greater than 5, is less than 6, or is 0
+ * print_alphabet_x10 - Determines either greater than 5, is less than 6, or is 0
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char al[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
 int j = 0;
 while (j < 10)
 {
-for (i = 0; i < 26; i++)
-{
-_putchar(al[0]);
-}
+print_alphabet();
 j++;
 }
 }
