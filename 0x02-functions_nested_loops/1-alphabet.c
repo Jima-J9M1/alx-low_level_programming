808b9734e7ff4f0113dc4c4bb30c13753136f125
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-15 17:24:15 +0300

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+int j = 0;
+while (j < 10)
+{
+for (i = 0; i < 26; i++)
+{
+_putchar(al[0]);
+}
+j++;
+}
