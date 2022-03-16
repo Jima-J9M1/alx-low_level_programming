b5db75c238139dcde825d4ba3bd47aec40c861b0
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-16 10:26:43 +0300

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_to_98 - print natural number from n to 98
+ *
+ * @n: number that will start to count
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+int i;
+if (n > 98)
+{
+for (i = n; i >= 98; i++)
+{
+_putchar(i + '0');
+}
+else
+{
+for (i = 0; i <= 98; i++)
+{
+_putchar(i + '0');
+_putchar(',');
+}
+}
