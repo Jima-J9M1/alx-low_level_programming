ca89ece559a7ee51186f853e26e8e314372d46af
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-16 09:33:13 +0300

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,13 @@
  */
 void times_table(void)
 {
-int i, j, k;
+int i, j;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-k = j * i;
-_putchar( (j * i)  + ('0'));
+
+_putchar((j * i)  + ('0'));
 if (j != 9)
 {
 _putchar(',');
