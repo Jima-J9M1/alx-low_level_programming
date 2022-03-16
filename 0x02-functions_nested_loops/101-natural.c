95deb8f06b05e904d1f7e16410585924612b9fa8
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-16 22:20:25 +0300

    commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-int n=0;
+int n = 0;
 int sum = 0;
-while (n >= 1024)
+while (n <= 1024)
 {
 if (n % 3 == 0 || n % 5 == 0)
 {
 sum += n;
 }
-n +=1;
+n += 1;
 }
 printf("%d\n", n);
 return (0);
