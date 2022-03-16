b479bcda43824e4ed83ef4bf056609575c636abe
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-16 08:43:46 +0300

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int n)
 	l = n % 10;
 	if (l < 0)
 	{
-		_putchar(-l);
+		_putchar(-l + '0');
 		return (-l);
 	}
 	else
 	{
-		_putchar(l);
+		_putchar(l + '0');
 		return (l);
 	}
 }
