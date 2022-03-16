044fcd84af30cd76a13605c634fcb436d8e2746f
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-16 22:46:17 +0300

    commmit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 int arr[50];
+int i;
 arr[0] = 1;
 arr[1] = 2;
 printf("%d, %d, ", arr[0], arr[1]);
-int i;
 for (i = 1; i < 50; i++)
 {
 arr[i + 1] = arr[i] + arr[i - 1];
 printf("%d, ", arr[i + 1]);
 }
-printf('\n');
+printf("\n");
 return (0);
 }
