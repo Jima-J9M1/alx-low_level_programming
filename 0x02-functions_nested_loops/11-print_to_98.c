7a1d6d7bf227edbf09f86816cc19d1aecf10638e
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-16 10:35:05 +0300

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,16 @@ void print_to_98(int n)
 int i;
 if (n > 98)
 {
-for (i = n; i >= 98; i++)
+for (i = n; i >= 98; i--)
 {
-_putchar(i + '0');
+printf("%d, ", i);
 }
 }
 else
 {
 for (i = 0; i <= 98; i++)
 {
-_putchar(i + '0');
-_putchar(',');
+printf("%d, ", i)
 }
 }
 }
