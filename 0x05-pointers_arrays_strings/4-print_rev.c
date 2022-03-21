0b58ff9164d9080ae7f96a6db93c7158d3471363
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-21 22:29:33 +0300

    comm

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - functoin that print the reverse of the string
+ * @s: parameter pointer
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+strrev(s);
+}
