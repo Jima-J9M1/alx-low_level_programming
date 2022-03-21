54e1f15f70cd44e64f9168527eb3bfb57cb53a4a
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-21 22:55:32 +0300

    comm

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - function that print the character of a string
+ * @str: parameter accept character
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+puts(str);
+}
