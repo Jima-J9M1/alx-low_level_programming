e0ee6966307ba821feef6e2b6bd332396258453d
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-28 22:28:58 +0300

    commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#include _MAIN_H
-#define _MAIN_H
+
+
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -12,4 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+
