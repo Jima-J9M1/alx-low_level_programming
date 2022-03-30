c4d16a2f23d85387a562b779567ce9626a5638a6
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-30 18:02:50 +0300

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - print the number of factorial
+ * @n: integer parameter
+ * Return: Always 0(Success)
+ */
+int factorial(int n)
+{
+int res = 1;
+if (n < 0)
+return (-1);
+else
+{
+res = n * factorial(n - 1);
+}
+return (res);
+}
