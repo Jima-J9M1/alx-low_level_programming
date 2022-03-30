ff443f2f070418458d874edcb08a80edfa72224b
Jima-J9M1 <Jimd3730@gmail.com>
2022-03-30 17:05:54 +0300

    commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+
 #define MAIN_H
 
 void _puts_recursion(char *s);
@@ -9,4 +9,3 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
-#endif
