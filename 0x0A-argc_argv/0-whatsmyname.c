ee52259dc8eea844e637328666a3c6db5c35aa0b
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-01 20:39:58 +0300

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that print the name of the files
+ * @argc: count Argument parameters
+ * @argv: Array Argument parametes
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	  printf("%s\n", argv[0]);
+}
