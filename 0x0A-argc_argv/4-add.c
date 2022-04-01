e2534d72e9d9f361a8a188acd22c82aef6322d38
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-01 22:33:29 +0300

    commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the sum of the arguments
+ * @argc: count the arguments
+ * @argv: arguments of the function
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int sum, i;
+for (i = 1; i <= argc - 1; i++)
+{
+if (!isdigit(argv[i])
+{
+printf("Error\n");
+return (1);
+}
+sum += argv[i];
+}
+return (sum);
+}
