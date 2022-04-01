26318ae1a30140b6bb8b5f664016a691d53c7992
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-01 21:32:09 +0300

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that print the number of the arguments
+ * @argc: number of argument
+ * @argv: vector arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
