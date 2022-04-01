5b147e48059dbc30e8196fab91d3232bbac5dce5
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-01 21:49:44 +0300

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that prints the two sum
+ * @argc: paramter of the function
+ * @argv: vector arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[1] + argv[2]);
+}
+return (0);
+}
