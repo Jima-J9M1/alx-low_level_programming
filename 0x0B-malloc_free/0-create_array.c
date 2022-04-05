9106e833289df9ae5a940b84b7791302ea1c4d29
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-05 21:15:14 +0300

    uploding create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,14 @@ char *create_array(unsigned int size, char c)
 char *ptr;
 int i;
 ptr = malloc(size * sizeof(char));
-
+if (size == 0)
+return (NULL);
+else
+{
 for (i = 0; i < n; i++)
 {
 ptr[i] = c;
 }
 return (ptr);
+}
 }
