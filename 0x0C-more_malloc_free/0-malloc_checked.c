8aeb96e2a678beb6c10dfebb40a136e1d60fa0db
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-08 10:24:34 +0300

    upload memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * malloc_checked - allocate a memory for different data types
+ * @b: sizeof the memory
+ * Return: Always 0 (Success)
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+if (b > 0 && b < INT_MAX)
+{
+ptr = malloc(b);
+}
+else
+{
+ptr = 98;
+}
+return (ptr);
+}
