fe5e40e8e7819f77005d77eda49175d42d599564
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-08 10:34:35 +0300

    upload memory allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,7 @@
  * malloc_checked - allocate a memory for different data types
  * @b: sizeof the memory
  * Return: Always 0 (Success)
+ * if malloc fails, status value is equal to 98
  */
 void *malloc_checked(unsigned int b)
 {
