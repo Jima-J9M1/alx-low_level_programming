f48919058e43fd413110f02c5ac891961820b036
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-14 10:14:43 +0300

    update files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,10 @@
 #include "function_pointers.h"
+
 /**
  * print_name - print name
  * @name: string parameter
  * @f: function pointer
+ *
  * Return: Always 0 (Success)
  */
 void print_name(char *name, void(*f)(char *))
