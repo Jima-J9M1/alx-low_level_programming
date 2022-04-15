a7fc6a5a89622df2edeaec1f3455cd19a8d68fac
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-15 23:14:54 +0300

    upload file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,13 +7,8 @@
  *
  * Return: Always 0 (Success)
  */
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	int i = 0;
-
-	while (name[i] != '\0')
-	{
-		_putchar(*(name + i));
-		i += 1;
-	}
+	if (name && f)
+		f(name);
 }
