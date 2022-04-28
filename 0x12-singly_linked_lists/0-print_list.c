a4f516dddeaf8accaec92e27d6e265e7941f4d21
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-28 16:51:26 +0300

    upload singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_list unsigned function that return Positive integer
+ * print_list - unsigned function that return Positive integer
  * @h: structure
  * Return: Always Success (0)
  */
-size_t print_list (const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
+
+
 	while (h)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 		{
-			printf("[%d] %s\n",0,"(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("[%d] %s\n",h->len,h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		}
-                h = h->next;
+		h = h->next;
 		count++;
 	}
 	return (count);
 }
-   
- 
