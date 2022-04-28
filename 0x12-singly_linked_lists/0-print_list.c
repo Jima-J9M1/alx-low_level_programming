94edc1d9dd5dbc646b3ca8b0c248089d1b5799e7
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-28 16:41:16 +0300

    upload singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list unsigned function that return Positive integer
+ * @h: structure
+ * Return: Always Success (0)
+ */
+size_t print_list (const list_t *h)
+{
+	size_t counter = 0;
+	while (h)
+	{
+		if(h->str == NULL)
+		{
+			printf("[%d] %s\n",0,"(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n",h->len,h->str);
+
+		}
+                h = h->next;
+		count++;
+	}
+	return (count);
+}
+   
+ 
