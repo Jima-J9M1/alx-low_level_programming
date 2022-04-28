ef4d02eb60929cc5dc7a3a57d8eb95bcb06567d6
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-28 17:00:47 +0300

    upload singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * list_len - return the number of element in linked list
+ * @h: linked list
+ * Return: Always Success(0)
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while(h)
+	{
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
