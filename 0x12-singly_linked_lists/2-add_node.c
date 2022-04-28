73af7eb511a1669736562dfd5b0e6260b56d0752
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-28 17:35:29 +0300

    upload singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * add_node - return the address of newNode
  * @head: postion counter linked list
  * @str: string
  * Return: Always Success(0)
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
