5fca3315cb20575e1f24396b578cf452aac58f0c
Jima-J9M1 <Jimd3730@gmail.com>
2022-04-28 17:45:02 +0300

    upload singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
  * add_node - return the address of newNode
  * @head: postion counter linked list
@@ -10,18 +9,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
-	list_t *temp;
+	size_t i;
 
 	newNode = malloc(sizeof(list_t));
-	newNode->str = strdup(str);
 
-	if (head == NULL)
-		head = &newNode;
-	else
-	{
-		temp = *head;
-		head = &newNode;
-		newNode->next = temp;
+	if (newNode == NULL)
+		return (NULL);
+	newNode->str = strdup(str);
+	while (str[i] != '\0')
+		i++;
+	newNode->len = i;
+	newNode->next = &head;
+	*head = newNode;
 	}
 	return (&newNode);
 }
